4164e90b294d9b47648abe0ef6c3a5e1e85041b0
hm-karius <karius@hm.edu>
2019-12-13 19:03:12 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    HomeComponent,
+    NavigationComponent,
+    CalendarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
